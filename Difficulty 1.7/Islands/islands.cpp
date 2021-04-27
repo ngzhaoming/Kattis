1f550ca4e232fc7f6a037260b38684051e255a74
ngzhaoming <ngzhaoming@gmail.com>
2021-04-27 14:18:42 +0800

    📝 Edit islands solution

--- Difficulty 1.7/Islands/islands.cpp
+++ Difficulty 1.7/Islands/islands.cpp
@@ -13,86 +13,87 @@ void printMap(vector<string> map) {
 }
 
 int countIsland(vector<string> map) {
-    vector<vector<bool> > mapBool;
     int row = map.size();
     int col = map[0].size();
 
-    for (int i = 0; i < map.size(); i++) {
-        vector<bool> currBool;
-        for (int j = 0; j < map[0].size(); j++) {
-            currBool.push_back(false);
-        }
-
-        mapBool.push_back(currBool);
-    }
-
     int islandTotal = 0;
+    queue<pair<int, int> > fillQueue;
 
     for (int i = 0; i < row; i++) {
         for (int j = 0; j < col; j++) {
             char currTile = map[i][j];
-            bool currTileBool = mapBool[i][j];
 
-            if (currTile == 'W') {
+            if (currTile == 'W' || currTile == 'N' || currTile == 'M') {
                  continue;
             }
 
-            if (currTileBool) {
+            if (currTile == 'C') {
+                fillQueue.push(make_pair(i, j));
+                bool isIslandMade = false;
+                
+                while (!fillQueue.empty()) {
+                    pair<int, int> currPair = fillQueue.front();
+                    fillQueue.pop();
+
+                    int k = currPair.first;
+                    int l = currPair.second;
+
+                    if (k < 0 || k >= row || l < 0 || l >= col) {
+                        continue;
+                    }
+
+                    char currCharTile = map[k][l];
+
+                    if (currCharTile == 'C') {
+                        map[k][l] = 'M';
+                        fillQueue.push(make_pair(k - 1, l));
+                        fillQueue.push(make_pair(k, l - 1));
+                        fillQueue.push(make_pair(k, l + 1));
+                        fillQueue.push(make_pair(k + 1, l));
+                    }
+
+                    if (currCharTile == 'L') {
+                        if (!isIslandMade) {
+                            islandTotal++;
+                            isIslandMade = true;
+                        }
+
+                        map[k][l] = 'N';
+                        fillQueue.push(make_pair(k - 1, l));
+                        fillQueue.push(make_pair(k, l - 1));
+                        fillQueue.push(make_pair(k, l + 1));
+                        fillQueue.push(make_pair(k + 1, l));
+                    }
+                }
+
                 continue;
             }
 
-            //Tile is a new land check all 4 position if there is land that has already been set to true
-            //Check all 4 adjacent positions, up, down, left, right
-            bool isNewIsland = true;
+            //Tile is Land ('L')
+            //Push into the queue the parent
+            fillQueue.push(make_pair(i, j)); // Push in the original
+            islandTotal++; // Change to noted and increase count
 
-            queue<pair<int, int> > fillQueue;
+            while (!fillQueue.empty()) {
+                pair<int, int> currPair = fillQueue.front();
+                fillQueue.pop();
 
-            for (int k = -1; k < 2; k++) {
-                for (int l = -1; l < 2; l++) {
-                    if (i + k < 0 || i + k >= row || j + l < 0 || j + l >= col) {
-                        continue;
-                    }
+                int k = currPair.first;
+                int l = currPair.second;
 
-                    if (k == -1 && l == -1) {
-                        continue;
-                    }
-
-                    if (k == -1 && l == 1) {
-                        continue;
-                    }
-
-                    if (k == 0 && l == 0) {
-                        continue;
-                    }
-
-                    if (k == 1 && l == -1) {
-                        continue;
-                    }
-
-                    if (k == 1 && l == 1) {
-                        continue;
-                    }
-
-                    char adjTile = map[i + k][j + l];
-                    bool verdict = mapBool[i + k][j + l];
-
-                    if (verdict) {
-                        isNewIsland = false;
-                    }
-
-                    if (adjTile == 'L' && !verdict) {
-                        mapBool[i + k][j + l] = true;
-                        pair<int, int> currPair;
-                        currPair.first = i + k;
-                        currPair.second = j + l;
-
-                        fillQueue.emplace(currPair)
-                    }
+                if (k < 0 || k >= row || l < 0 || l >= col) {
+                    continue;
                 }
-            }
 
-            if (isNewIsland) {
-                islandTotal++;
+                char currCharTile = map[k][l];
+
+                if (currCharTile == 'L' || currCharTile == 'C') {
+                    map[k][l] = 'N';
+                    fillQueue.push(make_pair(k - 1, l));
+                    fillQueue.push(make_pair(k, l - 1));
+                    fillQueue.push(make_pair(k, l + 1));
+                    fillQueue.push(make_pair(k + 1, l));
+                }
             }
         }
     }
@@ -103,8 +104,6 @@ int countIsland(vector<string> map) {
 int main() {
     int row, col;
     vector<string> original;
-    vector<string> water;
-    vector<string> land;
     cin >> row;
     cin >> col;
 
@@ -115,35 +114,9 @@ int main() {
         original.push_back(currStr);
     }
 
-    for (int i = 0; i < row; i++) {
-        string currStr = original[i];
-        string waterStr = "";
-        string landStr = "";
+    int islandTotal = countIsland(original);
 
-        for (int j = 0; j < col; j++) {
-            char currChar = currStr[j];
-
-            if (currChar == 'C') {
-                waterStr += 'W';
-                landStr += 'L';
-            } else {
-                waterStr += currChar;
-                landStr += currChar;
-            }
-        }
-
-        water.push_back(waterStr);
-        land.push_back(landStr);
-    }
-
-    int waterTotal = countIsland(water);
-    int landTotal = countIsland(land);
-
-    if (waterTotal < landTotal) {
-        cout << waterTotal << endl;
-    } else {
-        cout << landTotal << endl;
-    }
+    cout << islandTotal << endl;
 
     return 0;
 }
