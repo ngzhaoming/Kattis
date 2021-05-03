95b9bc6ce73c7b1e3e5e99a2c9273e77893e9d5b
ngzhaoming <ngzhaoming@gmail.com>
2021-05-03 09:17:31 +0800

    📝 Add difficulty 3.2 solution

---
+++ Difficulty 3.2/Alex and Barb/alex.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int k, m, n;
+    cin >> k;
+    cin >> m;
+    cin >> n;
+
+    if (k % (m + n) < m) {
+        cout << "Barb" << endl;
+    } else {
+        cout << "Alex" << endl;
+    }
+        
+    return 0;
+}
